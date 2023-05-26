4e7dd3937b9f1b6152b0e580d26765446aeb8c34
Ravi Bellam <ravi.elish@gmail.com>
2023-05-26 22:24:26 +0530

    Basic Layout changes

--- muit-feedo-app/src/App.js
+++ muit-feedo-app/src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import FeedoLayout from './components/view/FeedoLayout';
+
 function App() {
   return (
-    <div className="App">
-
-      Simple application for catpuring the feedbacks.   Branch1  
-
-    </div>
+    <>
+      <FeedoLayout></FeedoLayout>
+    </>
   );
 }
 
