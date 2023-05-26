221ad2f353f0c92cd973896edc5f0f756ddbb7c9
Ravi Bellam <ravi.elish@gmail.com>
2023-05-26 22:26:18 +0530

    Basic Layout changes (#5)

--- muit-feedo-app/src/App.js
+++ muit-feedo-app/src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
+import FeedoLayout from './components/view/FeedoLayout';
+
 function App() {
   return (
+
     <div className="App">
 
       Simple application for catpuring the feedbacks.   Branch1  Praveen kumar accepted your invitations
       Adding branch
 
     </div>
+
   );
 }
 
