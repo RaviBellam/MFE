aa13aa74183476b46c9f64123c45ae4aaf17a6a0
praveenRamavathu <praveennaikdee@gmail.com>
2023-06-14 11:59:18 -1100

    Forgot pass button

--- muit-feedo-app/src/components/view/LoginView.js
+++ muit-feedo-app/src/components/view/LoginView.js
@@ -10,7 +10,7 @@ function LoginView() {
   return (
     <>
     <div >
-    <Container className= "col-md-1 mx-auto" >
+    <Container className= "col-md-2 mx-auto" >
       <Row>
         <Col xs={10} md={8}>
           <Image src="https://www.pngfind.com/pngs/m/70-704328_edit-page-icon-transparent-index-icon-hd-png.png" thumbnail/>
@@ -18,9 +18,9 @@ function LoginView() {
           </Col>
       </Row>
     </Container>
-    <div className= "col-md-2 mx-auto">User name or email address</div> 
+    <div className= "col-md-4 mx-auto">User name or email address</div> 
     </div>
-    <div className = "feedo-border col-md-6 mx-auto">
+    <div className = "feedo-border col-md-8 mx-auto">
       <Form className="backtheme">
         <Form.Group  controlId="feedoMail">
           <Form.Label>User name or email address</Form.Label>
@@ -35,7 +35,7 @@ function LoginView() {
             <Row>
               <Col>Password</Col>
               <Col></Col>
-              <Col>Forgot password?</Col>
+              <Col> <Button variant="light">Forgot password?</Button></Col>
             </Row>       
           </Container> 
           <Form.Control type="password" placeholder="Enter password" /> <br />
